2c442bbdbc5d832879ecf1df1a4da518accfe80d
lloydchang <lloydchang@gmail.com>
2024-10-06 18:12:42 -0700

    fix

--- next.config.js
+++ next.config.js
@@ -6,22 +6,8 @@ const nextConfig = {
         source: "/api/py/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/:path*"
-            : "/api/",
-      },
-      {
-        source: "/docs",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/docs"
-            : "/api/py/docs",
-      },
-      {
-        source: "/openapi.json",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/openapi.json"
-            : "/api/py/openapi.json",
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "https://nextjs-fastapi-starter-navy-six.vercel.app/api/:path*",
       },
     ];
   },
